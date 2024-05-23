3c78d3dd258a78d673c22357b907e691fac954b3
ahmadkais <ahmadqais1997@gmail.com>
2024-05-24 00:51:26 +0300

    i dont know what i am doing any  more

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,17 @@
 #!/bin/bash
 
+cd ~/app
+
+
+if [ ! -d "venv" ]; then
+  python3 -m venv venv
+fi
+
+source venv/bin/activate
+
+pip install -r requirements.txt
+
+nohup venv/bin/python app.py &
+
+deactivate
 
