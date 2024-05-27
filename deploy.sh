9c7f62c110aca74c6ba9e77e723ca7ae3257d22c
ahmadkais <ahmadqais1997@gmail.com>
2024-05-27 14:01:47 +0300

    added a flag or not asking

--- deploy.sh
+++ deploy.sh
@@ -1,15 +1,13 @@
-#!/bin/bash
-
 cd ~/app/NetflixMovieCatalog
 
 if [ ! -d "venv" ]; then
-  python3 -m venv venv
+  python -m venv venv
 fi
-######!!!!123
+
 source venv/bin/activate
 
 pip install -r requirements.txt
-
+########
 deactivate
 
 sudo systemctl restart uwsgi
